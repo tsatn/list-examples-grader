c547d77c760cbdc0ae03bfe910c0ec2244101d05
tsatn <114328188+tsatn@users.noreply.github.com>
2022-11-28 11:17:04 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,9 +1,32 @@
-# Create your grading script here
+#clone to local
+if [ -d 'student-submission' ]
+then
+	rm -rf student-submission
+	echo 'old submission purged'
+fi
 
-rm -rf student-submission
-git clone $1 student-submission
-echo 'Finished cloning'
+if [ ! $# -eq 0 ]
+then
+	git clone $1 student-submission
+	echo 'finished cloning'
+else
+	echo 'no repo supplied'
+fi
 
+#check file exist
+if [ ! -e 'student-submission/ListExamples.java' ]
+then
+	echo 'ListExamples.java missing from submission'
+	exit
+fi
 
+#copy test to submission folder
+cp TestListExamples.java student-submission 
+echo "setup test cases"
 
+#compile
+set -e
+javac -classpath "lib/*" -d student-submission student-submission/*.java
+echo "Succesfully Compiled"
 
+#run junit
