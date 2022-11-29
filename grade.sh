2a4c49941303b235408c96a311fcc14726bb2b95
tsatn <114328188+tsatn@users.noreply.github.com>
2022-11-29 01:28:35 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,17 +1,11 @@
-#clone to local
-if [ -d 'student-submission' ]
-then
-	rm -rf student-submission
-	echo 'old submission purged'
-fi
 
-if [ ! $# -eq 0 ]
-then
-	git clone $1 student-submission
-	echo 'finished cloning'
-else
-	echo 'no repo supplied'
-fi
+rm -rf student-submission
+
+
+#clone to local
+git clone $1 student-submission
+echo 'finished cloning'
+
 
 #check file exist
 if [ ! -e 'student-submission/ListExamples.java' ]
@@ -30,3 +24,13 @@ javac -classpath "lib/*" -d student-submission student-submission/*.java
 echo "Succesfully Compiled"
 
 #run junit
+echo "Running tests..."
+java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > error.txt
+
+cat error.txt
+faliure =$(grep -i "Failures:" error.txt)
+
+if[[$faliure == ""]]
+then 
+echo "You are Good, all test passd"
+fi
